c2e357a41947bf66e3b703eb7927072b2ce662ec
rajaz26 <rajazain360.rz@gmail.com>
2024-02-05 19:36:43 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * @format
+ */
+import 'react-native-reanimated';
+import 'react-native-gesture-handler';
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from './src/amplifyconfiguration.json';
+Amplify.configure(amplifyconfig);
+AppRegistry.registerComponent(appName, () => App);
+
